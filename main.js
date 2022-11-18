270e3f320bd1734c9697afd7258b2ec57994b8ff
lisamarieandersson <lisamarie.andersson@gmail.com>
2022-11-18 14:27:49 +0100

    changes to all files and added image folder

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 window.addEventListener("DOMContentLoaded", main);
 
+/** @type {HTMLParagraphElement} h-tag för att ändra rubriken i varje scen */
+let headline;
 /** @type {HTMLParagraphElement} p-tag för att ändra texten i varje scen */
 let text;
 /** @type {HTMLButtonElement} val 1 i alla scener */
 let button1; 
-/** @type {HTMLButtonElement} val 2 i alla scener utom slutscen 2*/
+/** @type {HTMLButtonElement} val 2 i alla scener utom slutscen 2 */
 let button2; 
 
+/**
+ * Här startar programmet
+ */
 function main() {
   loadHtmlElements();
   loadStartScene();
 }
-
+/**
+ * Här laddas alla html-element
+ */
 function loadHtmlElements() {
+  headline = document.getElementById('headline');
   text = document.getElementById('text');
   button1 = document.getElementById('opt-1');
   button2 = document.getElementById('opt-2');
 }
 
+/**
+ * Funktionen ska... Syfte... 
+ */
 function loadStartScene() {
-  text.textContent = `Hello and welcome!
-    In this world you are faced with different scenarios
+  headline.textContent = `Hello and welcome!`;
+  text.textContent = `In this world you are faced with different scenarios
     in the novel Station Eleven by Emily St. John Mandel.
     Where do you want to go? And what choices do you want to make?`;
   
@@ -32,7 +43,14 @@ function loadStartScene() {
   button2.classList.remove('hidden');
 }
 
+/**
+ * Funktionen ska... Syfte...
+ */
 function loadTheaterScene() {
+  document.body.style.backgroundImage = "url(./images/doctoreleven.jpeg)";
+
+  headline.textContent = `Hello and welcome!`;
+
   text.textContent = `You've now entered the theater to watch King Lear. 
     During the play your sister Siya texts you that a deadly flu is coming to your town. 
     Where do you want to move next?`;
@@ -44,7 +62,14 @@ function loadTheaterScene() {
   button2.onclick = loadEndSceneTwo;
 }
 
+/**
+ * Funktionen ska... Syfte... 
+ */
 function loadGroceryStoreScene() {
+  document.body.style.backgroundImage = "url('./images/galaxystationeleven.jpeg')";
+
+  headline.textContent = `Hello!`;
+
   text.textContent = `You've now gone to the grocery store. 
     You decide to fill 10 carts with groceries. 
     Where do you want to move next?`;
@@ -57,8 +82,11 @@ function loadGroceryStoreScene() {
 }
 
 function loadSevernCityAirportScene() {
-  text.textContent = `Hello and welcome to Severn City Airport! 
-    In the terminal you watch the news report about a deadly flu coming your way. 
+  //document.body.style.backgroundImage = null;
+
+  headline.textContent = `Welcome to Severn City Airport!`;
+
+  text.textContent = ` In the terminal you watch the news report about a deadly flu coming your way. 
     What's your next move?`;
   
   button1.textContent = "Stay at the airport";
@@ -69,6 +97,9 @@ function loadSevernCityAirportScene() {
 }
 
 function loadEndSceneOne() {
+
+  headline.textContent = `:(`;
+
   text.textContent = `Oh no, you won't make this on your own!`;
 
   button1.textContent = "Start over";
@@ -80,6 +111,10 @@ function loadEndSceneOne() {
 }
 
 function loadEndSceneTwo() {
+  document.body.style.backgroundImage  = "url(./images/stationeleven.jpeg)";
+
+  headline.textContent = `Yes!`;
+
   text.textContent = `You survived, you are now safe!`;
 
   button1.textContent = "Start over";
@@ -89,6 +124,9 @@ function loadEndSceneTwo() {
 }
 
 function loadEndSceneThree() {
+
+  headline.textContent = `:(`;
+
   text.textContent = `Oh no, you won't make this on your own!`;
 
   button1.textContent = "Start over";
